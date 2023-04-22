ecff08ad141476a2194049e181232b00d631664e
276259822 <276259822@qq.com>
2023-04-22 10:41:04 +0800

    invite

--- src/screens/room/components/footer/components/invite/index.vue
+++ src/screens/room/components/footer/components/invite/index.vue
@@ -10,8 +10,26 @@
     :show-close="false"
     :align-center="true"
   >
-    <Header title="会议号：335 1774 4914" borderBottom />
-    <div class="invite-body"></div>
+    <Header title="会议号：335 1774 4914" borderBottom>
+      <template #right>
+        <div class="invite-header">
+          <div class="close-btn" @click="() => onToggle(false)">
+            <i class="iconfont icon-close" />
+          </div>
+        </div>
+      </template>
+    </Header>
+    <div class="invite-body">
+      <div class="title">诸葛琴魔 邀请您加入使命召唤的个人会议室</div>
+      <div class="invite-item">
+        <p>会议号：</p>
+        <p>335 1774 4914</p>
+      </div>
+    </div>
+    <div class="invite-footer">
+      <el-button class="btn">复制全部信息</el-button>
+      <el-button class="btn" type="primary">复制会议号</el-button>
+    </div>
   </el-dialog>
 </template>
 
