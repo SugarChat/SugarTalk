1431cb0900e7b02d21d0294c1bafc4a7b0ae8804
276259822 <276259822@qq.com>
2023-06-28 14:42:33 +0800

    update

--- src/screens/home/index.vue
+++ src/screens/home/index.vue
@@ -1,23 +1,19 @@
 <template>
   <Header
     class="header"
-    borderBottom
     title="Sugar Talk"
     :is-destroy="true"
     :hideMaximizable="true"
-  >
-    <template #right>
-      <div class="header-right">
-        <el-tooltip effect="light" content="设置" placement="bottom">
-          <div class="settings" @click="gotoSettings">
-            <i class="iconfont icon-settings" />
-          </div>
-        </el-tooltip>
-        <UserInfo @logout="onLogout" />
-      </div>
-    </template>
-  </Header>
+  />
   <div class="container">
+    <div class="user-info">
+      <UserInfo @logout="onLogout" />
+      <el-tooltip effect="light" content="设置" placement="bottom">
+        <div class="settings" @click="gotoSettings">
+          <i class="iconfont icon-settings" />
+        </div>
+      </el-tooltip>
+    </div>
     <div class="meeting-btns">
       <JoinBtn title="加入会议" icon="icon-add" @click="onJoinMeeting" />
       <JoinBtn title="快速会议" icon="icon-quick" @click="onQuickMeeting" />
